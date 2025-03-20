215e7590368c4f7c446255b6480d81a6d800f62c
Vilas Rathod <Vilasrj958@gmail.com>
2025-03-20 15:49:53 +0530

    Update PhegonHotelApplicationTests.java

--- backend/src/test/java/com/vilas/hotelsilver9/PhegonHotelApplicationTests.java
+++ backend/src/test/java/com/vilas/hotelsilver9/PhegonHotelApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PhegonHotelApplicationTests {
+class HotelSilver9ApplicationTests {
 
 	@Test
 	void contextLoads() {
